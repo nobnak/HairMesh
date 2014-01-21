b65f222c67197b3a28b4dbb5fc9931bba9336a87
nobnak <nobu.nakata@gmail.com>
2014-01-21 23:44:16 +0900

    backup

---
+++ Stream/Assets/Scripts/Editor/PrismEditor.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Prism))]
+public class PrismEditor : Editor {
+	private Prism _prism;
+	private SerializedProperty _layerMesh;
+	private SerializedProperty _layers;
+	private bool _layersFoldout;
+
+	void OnEnable() {
+		_prism = target as Prism;
+		_layerMesh = serializedObject.FindProperty("layerMesh");
+		_layers = serializedObject.FindProperty("layers");
+	}
+
+	void OnSceneGUI() {
+		var interpolation = 10;
+		var dt = 1f / interpolation;
+		var splines = _prism.UpdateSplines();
+		foreach (var spl in splines) {
+			var vertices = new Vector3[(spl.counter - 1) * interpolation + 1];
+			var counter = 0;
+			var iLastLayer = spl.counter - 1;
+			for (var iLayer = 0; iLayer < iLastLayer; iLayer++) {
+				for (var i = 0; i < interpolation; i++) {
+					var p = spl.Interpolate(iLayer, dt * i);
+					vertices[counter++] = p;
+				}
+			}
+			var lastP = spl.Interpolate(iLastLayer - 1, 1f);
+			vertices[counter++] = lastP;
+			Handles.DrawPolyLine(vertices);
+		}
+	}
+
+	public override void OnInspectorGUI() {
+		serializedObject.Update();
+		EditorGUI.BeginChangeCheck ();
+
+		EditorGUILayout.PropertyField(_layerMesh);
+		_layersFoldout = EditorGUILayout.Foldout(_layersFoldout, "Layers");
+		if (_layersFoldout) {
+			_layers.arraySize = EditorGUILayout.IntField("Size", _layers.arraySize);
+			for (var i = 0; i < _layers.arraySize; i++) {
+				EditorGUILayout.PropertyField(_layers.GetArrayElementAtIndex(i));
+			}
+		}
+
+		if (EditorGUI.EndChangeCheck()) {
+			serializedObject.ApplyModifiedProperties();
+		}
+	}
+}
--- Stream/Assets/Scripts/Prism.cs
+++ Stream/Assets/Scripts/Prism.cs
@@ -18,40 +18,9 @@ public class Prism : MonoBehaviour {
 			for (var i = 0; i < nVertices; i++) {
 				var p = layerMesh.vertices[i];
 				p = tr.TransformPoint(p);
-				p = transform.InverseTransformPoint(p);
 				splines[i].Add(p);
 			}
 		}
 		return splines;
-	}
-
-	public Mesh UpdateMesh() {
-		var interpolation = 10;
-        
-		var dt = 1f / interpolation;
-		var vertices = new List<Vector3>();
-		var segments = new List<int>();
-		for (var iSpline = 0; iSpline < splines.Length; iSpline++) {
-			var spline = splines[iSpline];
-			for (var iLayer = 0; iLayer < (layers.Length - 1); iLayer++) {
-				for (var i = 0; i < interpolation; i++) {
-					var iVertex = vertices.Count;
-					var p = spline.Interpolate(iLayer, dt * i);
-					vertices.Add(p);
-					segments.Add(iVertex);
-					segments.Add(iVertex + 1);
-				}
-			}
-			var pTip = spline.Interpolate(layers.Length - 2, 1f);
-			vertices.Add(pTip);
-		}
-
-		var mf = GetComponent<MeshFilter>();
-		var mesh = mf.sharedMesh == null ? mf.sharedMesh = new Mesh() : mf.sharedMesh;
-		mesh.Clear();
-		mesh.vertices = vertices.ToArray();
-		mesh.SetIndices(segments.ToArray(), MeshTopology.Lines, 0);
-		mesh.bounds = new Bounds(Vector3.zero, 1e6f * Vector3.one);
-		return mesh;
 	}
 }
